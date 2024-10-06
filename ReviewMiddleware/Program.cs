669f61497c2b3bfc481391807f8205d98296b242
Thuận Nguyễn Phước <nguyenphuocthuan.work@gmail.com>
2024-10-07 02:19:09 +0700

    creating request delegate using Run() and Use()

--- ReviewMiddleware/Program.cs
+++ ReviewMiddleware/Program.cs
@@ -1,6 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.Use(async (context, next) => {
+    await context.Response.WriteAsync("Middleware 1!\n");
+    await next(context);
+    
+    await context.Response.WriteAsync("Middleware 1 finished!\n");
+});
+
+app.Use(async (context, next) => {
+    await context.Response.WriteAsync("Middleware 2!\n");
+    await next(context);
+    
+    await context.Response.WriteAsync("Middleware 2 finished!\n");
+});
+
+app.Run(async (context) => {
+    await context.Response.WriteAsync("Middleware 3!\n");
+    
+    await context.Response.WriteAsync("Middleware 3 finished!\n");
+});
 
 app.Run();
--- ReviewMiddleware/obj/Debug/net8.0/ReviewMiddleware.AssemblyInfo.cs
+++ ReviewMiddleware/obj/Debug/net8.0/ReviewMiddleware.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReviewMiddleware")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb2212b07bf3ace3186ca70e7ebeabd7569cf140")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c273c0ce82725faedcb3793c6a61180d4df34f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReviewMiddleware")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReviewMiddleware")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
